3f64b188e0809abb8598236db1de304ecfe0884b
Matthew Krager <matthewkrager@gmail.com>
2019-10-26 01:23:28 -0700

    frontend-backend communications

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+
+@app.route("/pounds", methods=["POST"])
+def pounds():
+    form = request.get_json(force=True)
+    print(form)
+    code = form["code"]
+    requests_per_day = form["requests_per_day"]
+
+    response = {}
+    response["pounds"] = 0 # TODO: alfredo(code, requests_per_day)
+    return jsonify(response)
 
 if __name__ == "__main__":
-  app.run()
+    CORS(app, resources={r"/*": {"origins": "*"}})
+    app.run()
