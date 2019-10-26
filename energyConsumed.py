439ffbbdd799f864f69982b2d12c20159fe5af7d
Alfredo G <agomez@g.hmc.edu>
2019-10-26 11:29:53 -0700

    Added specs feature

--- energyConsumed.py
+++ energyConsumed.py
@@ -1,9 +1,25 @@
 import subprocess
+import specs
+def calculate_power(total_time, inst_name):
 
-def calculate_power(total_time):
-	sum_of_avg_pow = 10
+	local_ram = 16
+	local_cores = 2
+	local_speed = 1.3 
+	local_power = 60 # found using RAPL
 
-	power = total_time*1.58*sum_of_avg_pow
+	if inst_name == "local":
+		num_cores, clock_speed, ram_gig = (local_cores, local_speed, local_ram)
+	else:
+		num_cores, clock_speed, ram_gig = specs.spec_dic[inst_name]
+
+
+
+	adj_time = float(total_time) / (clock_speed/local_speed)
+	adj_time = adj_time / (float(ram_gig)/local_ram)
+	adj_pow = local_power * (float(num_cores) / local_cores) 
+
+	power = adj_time*1.58*adj_pow / 1000
+	print(power)
 	return power
 
 def get_time(num_req, cmd):
@@ -28,9 +44,9 @@ def get_time(num_req, cmd):
 	time = time / 60
 	return time
 
-def CO2e(num_req, cmd):
+def CO2e(num_req, cmd, inst_name):
 	total_time = get_time(num_req, cmd) # profiling to find total time to run code
-	total_power  = calculate_power(total_time)
+	total_power  = calculate_power(total_time, inst_name)
 	carbon_emit = 0.954 * total_power
 	return carbon_emit
 
@@ -40,7 +56,7 @@ def main():
 	filename = "prof_test.py"
 	cmd = "python -m cProfile -s cumtime"
 	cmd = cmd + " " + filename
-	print(CO2e(number_of_requests, cmd))
+	print(CO2e(number_of_requests, cmd, "local"))
 	
 
 if __name__ == '__main__':
---
+++ prof_test.py
@@ -0,0 +1,22 @@
+# from line_profiler import LineProfiler
+import math
+
+a = 1
+for i in range(int(math.pow(100,2))):
+	a=i
+
+a = 1
+for i in range(int(math.pow(100,2))):
+	a=i
+
+a = 1
+for i in range(int(math.pow(100,2))):
+	a=i
+
+a = 1
+for i in range(int(math.pow(100,2))):
+	a=i
+# return a
+
+# lp = LineProfiler(f)
+# print(lp)
