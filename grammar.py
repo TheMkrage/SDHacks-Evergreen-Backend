fa123f6c54558c12ed8906a279b6f556aab419f0
Alfredo G <agomez@g.hmc.edu>
2019-10-27 01:17:02 -0700

    Change comparison

--- grammar.py
+++ grammar.py
@@ -157,7 +157,7 @@ def from_profile(list_of_lines):
 			print(full_line)
 			sugg = grammar_on_line(full_line, first_line, last_line)
 
-			a = content
+			a = full_line
 			b =  " ".join(sugg)
 
 			if([c for c in a if c.isalpha()] != [c for c in b if c.isalpha()]):
@@ -181,13 +181,13 @@ def from_profile(list_of_lines):
 
 	return suggestions
 
-# test = [
-# Line(1, 2, 3, 4, 5, "if len(A) == 0:", 2),
-# Line(2, 2, 3, 4, 5, "s = ''", 2),
-# Line(3, 2, 3, 4, 5, "for substring in list:", 2),
-# Line(4, 2, 3, 4, 5, "s += substring", 3),
-# Line(5, 2, 3, 4, 5, "if len(A) == 0:", 2),
-# Line(6, 2, 3, 4, 5, "def run()", 2)
-# ]
+test = [
+Line(1, 2, 3, 4, 5, "if len(A) == 0:", 2),
+Line(2, 2, 3, 4, 5, "s = ''", 2),
+Line(3, 2, 3, 4, 5, "for substring in list:", 2),
+Line(4, 2, 3, 4, 5, "s += substring", 3),
+Line(5, 2, 3, 4, 5, "if len(A) == 0:", 2),
+Line(6, 2, 3, 4, 5, "def run()", 2)
+]
 
-# print(from_profile(test))
+print(from_profile(test))
